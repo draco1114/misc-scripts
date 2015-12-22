bc7f321978655121ad0e6f7b22540884ddc05b79
draco1114 <draco1114@gmail.com>
2015-12-22 17:39:07 -0500

    Update azure-test.bash

--- azure-test.bash
+++ azure-test.bash
@@ -2,9 +2,11 @@
 
 readonly HOSTNAME=`hostname`
 readonly IP=`ifconfig | grep inet | grep -v inet6 | grep -v 127.0.0 | awk '{print $2}' | awk -F: '{print $2}'`
+readonly FILE='/var/tmp/ip_info'
 
-echo ${HOSTNAME} >> /var/tmp/ip_info
-echo ${IP} >> /var/tmp/ip_info
+if [ ! -e ${FILE} ]; then
+    touch ${FILE}
+fi
 
-
-exit 0
+echo ${HOSTNAME} >> ${FILE}
+echo ${IP} >> ${FILE}
